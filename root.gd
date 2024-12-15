e9cbb244c31e23763e8366855f2af15e6272e9a8
A4R7bluep <a4r7bluep@gmail.com>
2024-12-14 20:11:27 -0500

    selection shortcut

--- root.gd
+++ root.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var fileDialog = $FileDialog
 @onready var richLabel = $BoxContainer/HBoxContainer/RichTextLabel
 @onready var codeEdit = $BoxContainer/CodeEdit
-@onready var timer = $Timer
+@onready var timer = $SaveTimer
 @onready var help = $CanvasLayer
 @onready var shortcutShow = $CanvasLayer/ShortcutPanel/MarginContainer/ShortcutShow
 
@@ -33,72 +33,97 @@ func _on_close_pressed() -> void:
 
 # string.indent()?
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("file_save"):
+	if Input.is_action_pressed("block"):
+		codeEdit.editable = false
+	elif Input.is_action_just_released("block"):
+		codeEdit.editable = true
+	
+	if Input.is_action_just_pressed("undo"):
+		codeEdit.editable = true
+		codeEdit.undo()
+	
+	elif Input.is_action_just_pressed("file_save"):
 		var fileWrite = FileAccess.open(filePath, FileAccess.WRITE)
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, currentCol - 1, currentLine, currentCol)
 		
 		fileWrite.store_string(codeEdit.text)
 		fileWrite.close()
 		richLabel.text = "[center]%s - saved[/center]" % filePath
 		timer.start(5)
 	
-	if Input.is_action_just_pressed("file_open"):
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, max(currentCol - 1, 0), currentLine, currentCol)
-		
+	elif Input.is_action_just_pressed("file_open"):
 		fileDialog.visible = true
 	
-	if Input.is_action_just_pressed("caret_left"):
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, currentCol - 1, currentLine, currentCol)
-		codeEdit.set_caret_column(currentCol - 2)
+	elif Input.is_action_just_pressed("select_left"):
+		var selectCol = codeEdit.get_selection_to_column()
+		var selectLine = codeEdit.get_selection_to_line()
+		var caretCol = codeEdit.get_caret_column()
+		var caretLine = codeEdit.get_caret_line()
+		codeEdit.select(selectLine, selectCol, caretLine, caretCol - 1)
 	
-	if Input.is_action_just_pressed("caret_right"):
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, currentCol - 1, currentLine, currentCol)
-		codeEdit.set_caret_column(currentCol)
+	elif Input.is_action_just_pressed("select_right"):
+		var selectCol = codeEdit.get_selection_from_column()
+		var selectLine = codeEdit.get_selection_from_line()
+		var caretCol = codeEdit.get_caret_column()
+		var caretLine = codeEdit.get_caret_line()
+		codeEdit.set_selection_mode(TextEdit.SELECTION_MODE_SHIFT)
+		codeEdit.select(selectLine, selectCol, caretLine, caretCol + 1)
 	
-	if Input.is_action_just_pressed("caret_up"):
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, max(currentCol - 1, 0), currentLine, currentCol)
-		codeEdit.set_caret_line(currentLine - 1)
+	elif Input.is_action_just_pressed("select_up"):
+		var selectCol = codeEdit.get_selection_to_column()
+		var selectLine = codeEdit.get_selection_to_line()
+		var caretCol = codeEdit.get_caret_column()
+		var caretLine = codeEdit.get_caret_line()
+		codeEdit.select(selectLine, selectCol, caretLine - 1, caretCol)
 	
-	if Input.is_action_just_pressed("caret_down"):
-		var currentCol = codeEdit.get_caret_column()
-		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, currentCol - 1, currentLine, currentCol)
-		codeEdit.set_caret_line(currentLine + 1)
+	elif Input.is_action_just_pressed("select_down"):
+		var selectCol = codeEdit.get_selection_from_column()
+		var selectLine = codeEdit.get_selection_from_line()
+		var caretCol = codeEdit.get_caret_column()
+		var caretLine = codeEdit.get_caret_line()
+		codeEdit.set_selection_mode(TextEdit.SELECTION_MODE_SHIFT)
+		codeEdit.select(selectLine, selectCol, caretLine + 1, caretCol)
 	
-	if Input.is_action_just_pressed("caret_beginning_line"):
+	elif Input.is_action_just_pressed("caret_beginning_line"):
 		codeEdit.set_caret_column(0)
 	
-	if Input.is_action_just_pressed("caret_end_line"):
+	elif Input.is_action_just_pressed("caret_end_line"):
 		var currentLine = codeEdit.get_caret_line()
 		codeEdit.set_caret_column(len(codeEdit.get_line(currentLine)))
 	
-	if Input.is_action_just_pressed("caret_beginning_para"):
+	elif Input.is_action_just_pressed("caret_beginning_para"):
 		var currentLine = codeEdit.get_caret_line()
 		var begParaLine = find_para_beginning(currentLine)
 		codeEdit.set_caret_line(begParaLine)
 		codeEdit.set_caret_column(codeEdit.get_first_non_whitespace_column(begParaLine))
 	
-	if Input.is_action_just_pressed("caret_end_para"):
+	elif Input.is_action_just_pressed("caret_end_para"):
 		var currentLine = codeEdit.get_caret_line()
 		var endParaLine = find_para_end(currentLine)
 		codeEdit.set_caret_line(endParaLine)
 		codeEdit.set_caret_column(codeEdit.get_first_non_whitespace_column(endParaLine))
 	
-	if Input.is_action_just_pressed("help"):
+	elif Input.is_action_just_pressed("caret_left"):
+		codeEdit.deselect()
 		var currentCol = codeEdit.get_caret_column()
+		codeEdit.set_caret_column(currentCol - 1)
+	
+	elif Input.is_action_just_pressed("caret_right"):
+		codeEdit.deselect()
+		var currentCol = codeEdit.get_caret_column()
+		codeEdit.set_caret_column(currentCol + 1)
+	
+	elif Input.is_action_just_pressed("caret_up"):
+		codeEdit.deselect()
 		var currentLine = codeEdit.get_caret_line()
-		codeEdit.remove_text(currentLine, max(currentCol - 1, 0), currentLine, currentCol)
-		
+		codeEdit.set_caret_line(currentLine - 1)
+	
+	elif Input.is_action_just_pressed("caret_down"):
+		codeEdit.deselect()
+		var currentLine = codeEdit.get_caret_line()
+		codeEdit.set_caret_line(currentLine + 1)
+	
+	
+	if Input.is_action_just_pressed("help"):
 		help.visible = not help.visible
 
 
